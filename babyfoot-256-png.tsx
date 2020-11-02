4d28ae14146fcb0787a1b53809e9012685a257f8
Frédéric MARTINEZ <frederic.martinez@ph2m.com>
2020-11-02 18:26:33 +0100

    Add Babyfoot jitsi room + silent jacuzzi zone

---
+++ babyfoot-256-png.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="babyfoot-256-png" tilewidth="32" tileheight="32" tilecount="32" columns="8">
+ <image source="../../Downloads/babyfoot-256.png" trans="00063d" width="256" height="128"/>
+</tileset>
